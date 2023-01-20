e030f6f7a2c5e4b0a60523860efaedabf530c38d
Melika AghajanianSabagh <84022036+melika-aghajanian@users.noreply.github.com>
2023-01-20 00:40:07 -0800

    Add files via upload

---
+++ phase 2/SQL TABLES/stadium.sql
@@ -0,0 +1,13 @@
+insert into MY_TABLE (id, name, city, stadium_capacity, create_date, is_covered_stadium)
+values  ('S-107', 'Kaliningrad Stadium', 'Kaliningrad', 34000, '2018-05-12', true),
+        ('S-108', 'Kazan Arena', 'Kazan', 43000, '2013-07-06', false),
+        ('S-109', 'Luzhniki Stadium', 'Moscow', 78000, '1956-07-31', true),
+        ('S-110', 'Otkritie Arena', 'Moscow', 44000, '2014-09-05', false),
+        ('S-111', 'Nizhny Novgorod Stadium', 'Nizhny Novgorod', 43000, '2018-04-15', true),
+        ('S-112', 'Rostov Arena', 'Rostov-on-Don', 43000, '2017-05-13', false),
+        ('S-113', 'Krestovsky Stadium', 'Saint Petersburg', 64000, '2017-04-22', true),
+        ('S-114', 'Samara Arena', 'Samara', 42000, '2018-04-28', false),
+        ('S-115', 'Mordovia Arena', 'Saransk', 42000, '2018-04-21', false),
+        ('S-116', 'Fisht Olympic Stadium', 'Sochi', 44000, '2013-05-12', false),
+        ('S-117', 'Volgograd Arena', 'Volgograd', 44000, '2018-03-18', true),
+        ('S-118', 'Central Stadium', 'Yekaterinburg', 33000, '1957-02-22', true);
